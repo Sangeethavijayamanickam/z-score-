60d5b9fe6f465aca587ef0a3dfc86edfeab14f7f
Sangeetha <102223109+Sangeethavijayamanickam@users.noreply.github.com>
2022-03-27 10:44:06 +0530

    Add files via upload

---
+++ z-score.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+data = np.array([6, 7, 8, 12, 13, 13, 14, 15, 16, 19, 22])
+stats.zscore(data)
+
+
+# In[ ]:
+
+
+
+
